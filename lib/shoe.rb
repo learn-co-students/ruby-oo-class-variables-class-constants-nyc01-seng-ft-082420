dd4120ddc1966c33a729dad9716dd7b2ba356610
sdonohue0918 <sdonohue0918@gmail.com>
2020-08-27 09:51:46 -0400

    all rspec passing for OO Class Constants lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,45 @@
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+
+#   def initialize(brand)
+#     @brand = brand
+#   end
+
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+
+# end
+
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS = []
+  
+  
 
   def initialize(brand)
+    
+    
     @brand = brand
+    # brands_all << brand
+    # brands_all.uniq
+    
+    BRANDS << brand unless BRANDS.include?(brand)
+    
+    
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  # def self.all
+  #   BRANDS.uniq
+  # end 
 
 end
